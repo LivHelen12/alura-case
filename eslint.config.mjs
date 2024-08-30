1dddd96c654c568aa8682c5fab49bbd84d8f0bed
Livia <liviahelen025@gmail.com>
2024-08-30 17:09:13 -0300

    feat: create initial project structure.

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintPrettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+    {
+        files: ['**/*.js'],
+        languageOptions: { globals: globals.browser },
+    },
+    pluginJs.configs.recommended,
+    eslintPrettier,
+    {
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-undef': 'warn',
+            'prettier/prettier': 'error',
+        },
+        plugins: {
+            prettier: prettierPlugin,
+        },
+    },
+]
