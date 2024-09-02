1e35a6b70f49a0444297de2206ad3c7bbaaa706b
Livia <liviahelen025@gmail.com>
2024-09-01 21:27:22 -0300

    chore: fix prettier style.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,20 +4,20 @@ import eslintPrettier from 'eslint-config-prettier'
 import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
-    {
-        files: ['**/*.js'],
-        languageOptions: { globals: globals.browser },
+  {
+    files: ['**/*.js'],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  eslintPrettier,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'prettier/prettier': 'error',
     },
-    pluginJs.configs.recommended,
-    eslintPrettier,
-    {
-        rules: {
-            'no-unused-vars': 'warn',
-            'no-undef': 'warn',
-            'prettier/prettier': 'error',
-        },
-        plugins: {
-            prettier: prettierPlugin,
-        },
+    plugins: {
+      prettier: prettierPlugin,
     },
+  },
 ]
