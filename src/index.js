10d202013d1c68db2ab6f5ce5e70f5d1619e3373
Livia <liviahelen025@gmail.com>
2024-09-03 20:47:27 -0300

    feat: display the input value dynamically.

--- src/index.js
+++ src/index.js
@@ -1,110 +1,116 @@
-const spanPasswordEl = document.querySelector('#generated_password')
-const forceIndicatorEl = document.querySelector('.force_box')
-const inputRangeSliderEl = document.querySelector('#slider')
-const boxSlidesEl = document.querySelector('#box-range')
-const checkboxesEl = document.querySelectorAll('.checkbox')
-const checkboxUppercaseEl = document.querySelector('#uppercase')
-const checkboxLowercaseEl = document.querySelector('#lowercase')
-const checkboxNumberEl = document.querySelector('#number')
-const checkboxSymbolEl = document.querySelector('#symbols')
+const spanPasswordEl = document.querySelector('#generated_password');
+const forceIndicatorEl = document.querySelector('.force_box');
+const inputRangeSliderEl = document.querySelector('#slider');
+const boxRangeSliderEl = document.querySelector('#box-range');
+const checkboxesEl = document.querySelectorAll('.checkbox');
+const checkboxUppercaseEl = document.querySelector('#uppercase');
+const checkboxLowercaseEl = document.querySelector('#lowercase');
+const checkboxNumberEl = document.querySelector('#number');
+const checkboxSymbolEl = document.querySelector('#symbols');
 
-const lowercaseChars = 'abcçdefghijklmnopqrstuvwxyz'
-const uppercaseChars = 'ABCÇDEFGHIJKLMNOPQRSTUVWXYZ'
-const numberChars = '0123456789'
-const symbolChars = '!#$%&()*+,-./:;<=>?@[]^_{|}'
+const lowercaseChars = 'abcçdefghijklmnopqrstuvwxyz';
+const uppercaseChars = 'ABCÇDEFGHIJKLMNOPQRSTUVWXYZ';
+const numberChars = '0123456789';
+const symbolChars = '!#$%&()*+,-./:;<=>?@[]^_{|}';
 
-let password = ''
-let passwordLength = 12
-let baseChars = ''
+let password = '';
+let passwordLength = 12;
+let baseChars = '';
 
 function generatePassword() {
-  resetPassBeforeGenerate()
-  setPassword()
-  calculatePasswordForce()
+  resetPassBeforeGenerate();
+  setInputRangerValue();
+  setPassword();
+  calculatePasswordForce();
 }
 
 function setPassword() {
-  baseChars = ''
+  baseChars = '';
 
   if (checkboxLowercaseEl.checked) {
-    baseChars += lowercaseChars
+    baseChars += lowercaseChars;
   }
 
   if (checkboxUppercaseEl.checked) {
-    baseChars += uppercaseChars
+    baseChars += uppercaseChars;
   }
 
   if (checkboxNumberEl.checked) {
-    baseChars += numberChars
+    baseChars += numberChars;
   }
 
   if (checkboxSymbolEl.checked) {
-    baseChars += symbolChars
+    baseChars += symbolChars;
   }
 
   if (!baseChars || baseChars.length === '') {
-    return (spanPasswordEl.textContent = 'Personalize sua senha antes')
+    return (spanPasswordEl.textContent = 'Personalize sua senha antes');
   }
 
   for (let i = 0; i < passwordLength; i++) {
-    const randomNumber = Math.floor(Math.random() * baseChars.length)
-    password += baseChars[randomNumber]
+    const randomNumber = Math.floor(Math.random() * baseChars.length);
+    password += baseChars[randomNumber];
   }
 
-  spanPasswordEl.textContent = password
+  spanPasswordEl.textContent = password;
 }
 
 function calculatePasswordForce() {
-  let checkedCount = 0
+  let checkedCount = 0;
 
   checkboxesEl.forEach((checkbox) => {
     if (checkbox.checked) {
-      checkedCount++
+      checkedCount++;
     }
   })
 
-  forceIndicatorEl.classList.remove('low', 'medium', 'strong')
-  const DEFAULT_PASSWORD_LENGTH = 12
-  const MINIMIUM_ACCEPTABLE_PASSWORD_LENGTH = 18
+  forceIndicatorEl.classList.remove('low', 'medium', 'strong');
+  const DEFAULT_PASSWORD_LENGTH = 12;
+  const MINIMIUM_ACCEPTABLE_PASSWORD_LENGTH = 18;
 
   if (passwordLength < DEFAULT_PASSWORD_LENGTH || checkedCount <= 2) {
-    forceIndicatorEl.classList.add('low')
+    forceIndicatorEl.classList.add('low');
   } else if (
     (passwordLength >= DEFAULT_PASSWORD_LENGTH &&
       passwordLength <= MINIMIUM_ACCEPTABLE_PASSWORD_LENGTH) ||
     checkedCount === 3
   ) {
-    forceIndicatorEl.classList.add('medium')
+    forceIndicatorEl.classList.add('medium');
   } else if (
     passwordLength > MINIMIUM_ACCEPTABLE_PASSWORD_LENGTH &&
     checkedCount === 4
   ) {
-    forceIndicatorEl.classList.add('strong')
+    forceIndicatorEl.classList.add('strong');
   }
 }
 
 function resetPassBeforeGenerate() {
-  password = ''
+  password = '';
 }
 
 function copyPassword() {
-  return navigator.clipboard.writeText(spanPasswordEl.textContent)
+  return navigator.clipboard.writeText(spanPasswordEl.textContent);
+}
+
+function setInputRangerValue() {
+  passwordLength = inputRangeSliderEl.value;
+  boxRangeSliderEl.textContent = passwordLength;
 }
 
 inputRangeSliderEl.addEventListener('input', () => {
-  passwordLength = inputRangeSliderEl.value
-  boxSlidesEl.textContent = passwordLength
-  calculatePasswordForce()
-  generatePassword()
+  setInputRangerValue();
+  calculatePasswordForce();
+  generatePassword();
 })
 
 checkboxesEl.forEach((checkbox) => {
-  checkbox.addEventListener('change', calculatePasswordForce)
+  checkbox.addEventListener('change', calculatePasswordForce);
 })
 
-checkboxUppercaseEl.addEventListener('click', generatePassword)
-checkboxLowercaseEl.addEventListener('click', generatePassword)
-checkboxNumberEl.addEventListener('click', generatePassword)
-checkboxSymbolEl.addEventListener('click', generatePassword)
+checkboxUppercaseEl.addEventListener('click', generatePassword);
+checkboxLowercaseEl.addEventListener('click', generatePassword);
+checkboxNumberEl.addEventListener('click', generatePassword);
+checkboxSymbolEl.addEventListener('click', generatePassword);
+spanPasswordEl.addEventListener('click', copyPassword);
 
-generatePassword()
+generatePassword();
